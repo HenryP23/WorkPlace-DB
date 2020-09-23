0169efdda099ba7ab51bdc56b5c75ded8fb014ce
Henry Parrish <henryhparrish@gmail.com>
2020-09-22 19:37:54 -0500

    changed sql file

--- workplaceDB.sql
+++ workplaceDB.sql
@@ -1,38 +1,74 @@
 
-* **department**:
-
-  * **id** - INT PRIMARY KEY
-  * **name** - VARCHAR(30) to hold department name
-
-* **role**:
-
-  * **id** - INT PRIMARY KEY
-  * **title** -  VARCHAR(30) to hold role title
-  * **salary** -  DECIMAL to hold role salary
-  * **department_id** -  INT to hold reference to department role belongs to
-
-* **employee**:
-
-  * **id** - INT PRIMARY KEY
-  * **first_name** - VARCHAR(30) to hold employee first name
-  * **last_name** - VARCHAR(30) to hold employee last name
-  * **role_id** - INT to hold reference to role employee has
-  * **manager_id** - INT to hold reference to another employee that manages the employee being Created. This field may be null if the employee has no manager
-
-
 DROP DATABASE IF EXISTS workplaceDB;
 CREATE DATABASE workplaceDB;
 
 USE workplaceDB;
+
   CREATE TABLE department (
       id INT NOT NULL AUTO_INCREMENT,
-      name VARCHAR(100) AUTO_INCREMENT
+      dept VARCHAR(100),
+	  PRIMARY KEY(id)
   );
 
   CREATE TABLE role(
-    
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id),
+	PRIMARY KEY(id)
   );
 
-   CREATE TABLE (
-    
+   CREATE TABLE employee(
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT ,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+	PRIMARY KEY(id)
   );
+
+  INSERT INTO employee (first_name, last_name)
+VALUES ('Henry', 'Parrish');
+
+INSERT INTO role (title, salary)
+VALUES ('Sales Lead', 80000);
+
+INSERT INTO department (dept)
+VALUES ('Sales');
+
+INSERT INTO employee (first_name, last_name)
+VALUES ('Lily', 'Parrish');
+
+INSERT INTO role (title, salary)
+VALUES ('Software Engineer', 80000);
+
+INSERT INTO department (dept)
+VALUES ('Engineering');
+
+INSERT INTO employee (first_name, last_name, manager_id)
+VALUES ('Ben', 'Smith', 1);
+
+INSERT INTO role (title, salary)
+VALUES ('Salesman', 80000);
+
+INSERT INTO department (dept)
+VALUES ('Sales');
+
+INSERT INTO employee (first_name, last_name, manager_id)
+VALUES ('West', 'Mac', 1);
+
+INSERT INTO role (title, salary)
+VALUES ('Manager', 80000);
+
+INSERT INTO department (dept)
+VALUES ('Sales');
+INSERT INTO employee (first_name, last_name)
+VALUES ('Franklin', 'John');
+
+INSERT INTO role (title, salary)
+VALUES ('Manager', 80000);
+
+INSERT INTO department (dept)
+VALUES ('Sales');
